2aee032c9b09c872ce6b071325c387a4c89edb18
sahelhoseini <sahellhoseini99@yahoo.com>
2025-01-05 15:21:25 +0330

    create new page Home and router folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
