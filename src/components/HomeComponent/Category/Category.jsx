669fc0a6b1c2400e204823f3f04801e30c8aa34e
sahelhoseini <sahellhoseini99@yahoo.com>
2025-01-10 15:56:39 +0330

    add overflow-hidden to image for zoom when hover

--- src/components/HomeComponent/Category/Category.jsx
+++ src/components/HomeComponent/Category/Category.jsx
@@ -4,12 +4,12 @@ const Category = () => {
   return (
     <div className="w-full m-auto max-w-[1440px]">
       <div className="grid grid-cols-3 gap-1 md:gap-4 px-10 md:px-36">
-        <div className="col-span-1  w-full max-h-72 relative">
+        <div className="col-span-1 w-full max-h-72 relative overflow-hidden rounded-3xl">
           <Link to="/">
             <img
               src={window.location.origin + "/images/table.jpg"}
               alt=""
-              className="w-full h-full object-cover rounded-3xl  transition-transform duration-300 transform ease-in-out scale-100 hover:scale-105"
+              className="w-full h-full object-cover rounded-3xl transition-transform duration-300 transform ease-in-out scale-100 hover:scale-110" // افزایش مقدار تغییر اندازه
             />
             <p className="rounded-3xl bg-white px-1 py-1 md:text-xl text-xs absolute bottom-1 left-1">
               Living Room
@@ -17,12 +17,12 @@ const Category = () => {
           </Link>
         </div>
 
-        <div className="col-span-1  w-full max-h-72 relative">
+        <div className="col-span-1 w-full max-h-72 relative overflow-hidden rounded-3xl">
           <Link to="/">
             <img
               src={window.location.origin + "/images/bed.jpg"}
               alt=""
-              className="w-full h-full object-cover rounded-3xl  transition-transform duration-300 transform ease-in-out scale-100 hover:scale-105"
+              className="w-full h-full object-cover rounded-3xl transition-transform duration-300 transform ease-in-out scale-100 hover:scale-110"
             />
             <p className="rounded-3xl bg-white px-1 py-1 md:text-xl text-xs absolute bottom-1 left-1">
               Bed Room
@@ -30,13 +30,13 @@ const Category = () => {
           </Link>
         </div>
 
-        <div className="col-span-1  flex flex-col gap-1 md:gap-2">
-          <div className=" max-h-36 relative">
+        <div className="col-span-1 flex flex-col gap-1 md:gap-2">
+          <div className="max-h-36 relative overflow-hidden rounded-3xl">
             <Link to="/">
               <img
                 src={window.location.origin + "/images/baby.jpg"}
                 alt=""
-                className="w-full h-full object-cover rounded-3xl  transition-transform duration-300 transform ease-in-out scale-100 hover:scale-105"
+                className="w-full h-full object-cover rounded-3xl transition-transform duration-300 transform ease-in-out scale-100 hover:scale-110"
               />
               <p className="rounded-3xl bg-white px-1 py-1 md:text-xl text-xs absolute bottom-1 left-1">
                 Kids Room
@@ -44,12 +44,12 @@ const Category = () => {
             </Link>
           </div>
 
-          <div className=" max-h-36 relative">
+          <div className="max-h-36 relative overflow-hidden rounded-3xl">
             <Link to="/">
               <img
                 src={window.location.origin + "/images/kitchen.jpg"}
                 alt=""
-                className="w-full h-full object-cover rounded-3xl  transition-transform duration-300 transform ease-in-out scale-100 hover:scale-105"
+                className="w-full h-full object-cover rounded-3xl transition-transform duration-300 transform ease-in-out scale-100 hover:scale-110"
               />
               <p className="rounded-3xl bg-white px-1 py-1 md:text-xl text-xs absolute bottom-1 left-1">
                 Kitchen
