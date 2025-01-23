d302a93087773c80bccdcd02cf251c8e5fd02917
sahelhoseini <sahellhoseini99@yahoo.com>
2025-01-23 11:28:00 +0330

    add profile page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from "./Home/Home";
 export { default as Shop } from "./Shop/Shop";
 export { default as Product } from "./Product/Product";
+export { default as Profile } from "./Profile/Profile";
